30781f2c66fa9cdbee45a017c0f1f2a44f28de2a
lmpassalacqua <pas949jmp>
2021-01-30 14:58:13 -0300

    Set up spring boot

---
+++ src/main/java/com/wolox/wchallenge/WChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.wolox.wchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wolox/wchallenge/WChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolox.wchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
