b0d907365568369a19a6f2a528839f3f60696807
lmpassalacqua <pas949jmp>
2021-02-01 11:06:30 -0300

    Validaciones en el endpoint de insert y update

--- src/main/java/com/wolox/wchallenge/controller/SharedAlbumController.java
+++ src/main/java/com/wolox/wchallenge/controller/SharedAlbumController.java
@@ -1,6 +1,7 @@
 package com.wolox.wchallenge.controller;
 
 import com.wolox.wchallenge.ApiConfig;
+import com.wolox.wchallenge.model.Permission;
 import com.wolox.wchallenge.model.SharedAlbum;
 import com.wolox.wchallenge.service.SharedAlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -32,14 +34,55 @@ public class SharedAlbumController {
 
     @PostMapping(value = "")
     public ResponseEntity<SharedAlbum> postSharedAlbums(@RequestBody SharedAlbum sharedAlbum){
+
+        String errorMessange = "";
+        if (!validateFields(sharedAlbum)) {
+            errorMessange += "ERROR: Hay campos vacíos. Los campos obligatorios son: ";
+            errorMessange += " userId, albumId, y permission (" + Arrays.asList(Permission.values()) + ") ";
+            return new ResponseEntity(errorMessange, HttpStatus.BAD_REQUEST);
+        } else if (!validateInsert(sharedAlbum)) {
+            errorMessange += "ERROR: Ya existe un album compartido para albumId: " + sharedAlbum.getAlbumId() ;
+            errorMessange += " y userId: " + sharedAlbum.getUserId();
+            return new ResponseEntity(errorMessange, HttpStatus.BAD_REQUEST);
+        }
+
         sharedAlbum = sharedAlbumService.save(sharedAlbum);
         return new ResponseEntity<>(sharedAlbum, HttpStatus.CREATED);
     }
 
+    private boolean validateInsert(SharedAlbum sharedAlbum) {
+        boolean valid = true;
+        List<SharedAlbum> sharedAlbumList = sharedAlbumService.findAll();
+        for (SharedAlbum item : sharedAlbumList) {
+            if (item.getAlbumId().equals(sharedAlbum.getAlbumId())
+                    && item.getUserId().equals(sharedAlbum.getUserId())) {
+                valid = false;
+            }
+        }
+        return valid;
+    }
+
+    private boolean validateFields(SharedAlbum sharedAlbum) {
+        boolean valid = true;
+
+        if (sharedAlbum.getAlbumId() == null || sharedAlbum.getUserId() == null
+                || sharedAlbum.getPermission() == null) {
+            valid = false;
+        }
+        return valid;
+    }
+
     @PutMapping(value = "")
     public ResponseEntity<SharedAlbum> putSharedAlbums(@RequestBody SharedAlbum sharedAlbum){
-        List<SharedAlbum> sharedAlbumList = sharedAlbumService.findAll();
 
+        String errorMessange = "";
+        if (!validateFields(sharedAlbum)) {
+            errorMessange += "ERROR: Hay campos vacíos. Los campos obligatorios son: ";
+            errorMessange += " userId, albumId, y permission (" + Arrays.asList(Permission.values()) + ") ";
+            return new ResponseEntity(errorMessange, HttpStatus.BAD_REQUEST);
+        }
+
+        List<SharedAlbum> sharedAlbumList = sharedAlbumService.findAll();
         for (SharedAlbum item : sharedAlbumList) {
             if (item.getAlbumId().equals(sharedAlbum.getAlbumId())
                         && item.getUserId().equals(sharedAlbum.getUserId())){
--- src/main/java/com/wolox/wchallenge/model/Permission.java
+++ src/main/java/com/wolox/wchallenge/model/Permission.java
@@ -1,5 +1,7 @@
 package com.wolox.wchallenge.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public enum Permission {
     READ,
     READ_AND_WRITE;
