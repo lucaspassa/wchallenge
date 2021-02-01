957d5e1b0d4ee100d7e5d3f80a63f048d0cf6d63
lmpassalacqua <pas949jmp>
2021-02-01 11:36:17 -0300

    Se plantean los test

--- src/test/java/com/wolox/wchallenge/WChallengeApplicationTests.java
+++ src/test/java/com/wolox/wchallenge/WChallengeApplicationTests.java
@@ -1,13 +1,53 @@
 package com.wolox.wchallenge;
 
+import com.wolox.wchallenge.controller.ExternalApiController;
+import com.wolox.wchallenge.model.Album;
+import com.wolox.wchallenge.model.Comment;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class WChallengeApplicationTests {
 
+	@Autowired
+	private ExternalApiController externalApiController;
+
 	@Test
 	void contextLoads() {
+		assertThat(externalApiController).isNotNull();
+	}
+
+	@Test
+	void getAlbumsTest() {
+		ResponseEntity<List<Album>> albums = externalApiController.getAlbums();
+		assertThat(albums).isNotNull();
+	}
+
+	@Test
+	void getAlbumsByUserTest() {
+		ResponseEntity<Object> albumsByUser = externalApiController.getAlbumsByUser("1");
+		assertThat(albumsByUser).isNotNull();
+	}
+
+	@Test
+	void getCommentsTest() {
+		ResponseEntity<List<Comment>> comments = externalApiController.getComments(null);
+		assertThat(comments).isNotNull();
+
+		ResponseEntity<List<Comment>> commentsWithName = externalApiController.getComments("a");
+		assertThat(commentsWithName).isNotNull();
+
+		ResponseEntity<List<Comment>> commentsNoResults = externalApiController.getComments("prueba de contenido que no figura");
+		assertThat(commentsNoResults).isNotNull();
+
+		List<Comment> resultsBody = commentsNoResults.getBody();
+		assertThat(resultsBody.size()).isEqualTo(0);
 	}
 
 }
